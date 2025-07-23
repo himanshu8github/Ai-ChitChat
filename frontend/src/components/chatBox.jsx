8e5c959a0855262f8d388e86676630d56e8e694f
Himanshu <himanshukakran8@gmail.com>
2025-07-23 15:44:17 +0530

    fix issue of backend url

--- frontend/src/components/chatBox.jsx
+++ frontend/src/components/chatBox.jsx
@@ -27,7 +27,7 @@ const ChatBox = () => {
     if (!message.trim()) return;
 
     try {
-     axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/chat`, { message });
+     axios.post(`${process.env.VITE_BACKEND_URL}/api/chat`, { message });
 
 
       setChat((prevChat) => [
