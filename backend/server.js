4815331abb6cc4823b74cdfd9a04f67808a34e44
Himanshu <himanshukakran8@gmail.com>
2025-07-24 13:13:21 +0530

    fix error in Gemini Api

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,10 @@ app.use('/api/user', authRoutes);
 
 
 
-mongoose.connect(process.env.MONGODB_URI, {
-})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error("MongoDB connection error:", err));
+// mongoose.connect(process.env.MONGODB_URI, {
+// })
+// .then(() => console.log("MongoDB connected"))
+// .catch(err => console.error("MongoDB connection error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
